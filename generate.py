722142439e562b1fbf9f4255b029580c1ac00e7e
Filip.C <cfilip002@gmail.com>
2024-02-06 00:41:17 +0100

    directory creating and prompts separated

--- generate.py
+++ generate.py
@@ -1,25 +1,36 @@
 import os
 
-tehnical_name = str(input("Enter the tehnical name of your module: "))
+
+
 directroy_list = os.listdir()
 main_directory = os.getcwd()
 
-if tehnical_name not in directroy_list:
-    os.mkdir(f"{tehnical_name}")
-    os.chdir(f"{main_directory}\{tehnical_name}")
-    
-    while True:
-        models_answer = input("Do you want to create a Models folder (Y/N): ")
-
-        if models_answer.lower() == "y":
-            os.mkdir("models")
-            break
-        elif models_answer.lower() == "n":
-            print("Skipping Models creation")
-            break
-        else:
-            print("Please enter a valid answer Y - yes or N - no!")
-else:
-    print("A module with this name already exists!")
+generateOptions = {}
 
 
+
+while True:
+    tehnical_name = str(input("Enter the tehnical name of your module: "))
+
+    if tehnical_name not in directroy_list:
+        generateOptions["technical_name"] = tehnical_name 
+
+        while True:
+            models_answer = input("Do you want to create a Models folder (Y/N): ")
+            if models_answer.lower() == "y":
+                generateOptions["models"] = True
+                break
+            elif models_answer.lower() == "n":
+                print("Skipping Models creation")
+                generateOptions["models"] = False
+                break
+            else:
+                print("Please enter a valid answer Y - yes or N - no!")
+        break
+    else:
+        print("A module with this name already exists!")
+
+
+os.mkdir(generateOptions.get("technical_name"))
+os.chdir(os.path.join(main_directory, generateOptions.get("technical_name")))
+if generateOptions["models"] : os.mkdir("models")
